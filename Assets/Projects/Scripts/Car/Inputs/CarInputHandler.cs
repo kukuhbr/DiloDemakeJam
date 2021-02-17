1d7bf8e75c100d81d6b5d1346110a4dcfe591629
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2021-02-18 03:36:09 +0700

    Add Handle Input function

--- Assets/Projects/Scripts/Car/Inputs/CarInputHandler.cs
+++ Assets/Projects/Scripts/Car/Inputs/CarInputHandler.cs
@@ -2,44 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using NFS.Car.Inputs.Commands;
+
 namespace NFS.Car.Inputs
 {
     public class CarInputHandler : MonoBehaviour
     {
+        private Command bAccel;
+        private Command bNothing;
+        private Command bHBrake;
+        private Command bNos;
+        private Command bSGear;
+        private Command bSteer;
 
         // Start is called before the first frame update
         void Start()
         {
-
+            bAccel = new Accelerate();
+            bNothing = new DoNothing();
+            bHBrake = new HandBrake();
+            bNos = new Nos();
+            bSGear = new ShiftGear();
+            bSteer = new Steer();
         }
 
         // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
             HandleInput();
         }
 
         private void HandleInput()
         {
+            HandleShiftGearInput();
+            HandleHandBrakeInput();
+            HandleNosInput();
+            HandleAccelerateInput();
+            HandleSteerInput();
+        }
+
+        private void HandleAccelerateInput()
+        {
             if (IsInputAccelerate())
             {
-
+                bAccel.Execute();
             }
+        }
+
+        private void HandleShiftGearInput()
+        {
+            if (IsInputShiftGear())
+            {
+                bSGear.Execute();
+            }
+        }
+
+        private void HandleSteerInput()
+        {
             if (IsInputSteer())
             {
-
+                bSteer.Execute();
             }
-            if (IsInputHandBrake())
-            {
+        }
 
-            }
+        private void HandleNosInput()
+        {
             if (IsInputNos())
             {
-
+                bNos.Execute();
             }
-            if (IsInputSteer())
-            {
+        }
 
+        private void HandleHandBrakeInput()
+        {
+            if (IsInputHandBrake())
+            {
+                bHBrake.Execute();
             }
         }
 
