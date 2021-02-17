32917a4e3ab740da35bc55485bbffc1518a69805
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2021-02-17 21:28:20 +0700

    Add empty command classes

---
+++ Assets/Projects/Scripts/Car/Inputs/Commands/Car Commands/DoNothing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFS.Car.Inputs.Commands
+{
+    public class DoNothing : Command
+    {
+        public override void Execute()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Projects/Scripts/Car/Inputs/Commands/Car Commands/Nos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFS.Car.Inputs.Commands
+{
+    public class Nos : Command
+    {
+        public override void Execute()
+        {
+
+        }
+    }
+}
