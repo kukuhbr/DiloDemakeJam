a940aa23b0459f142c19bd4b691c14a324f44684
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2021-02-18 16:23:44 +0700

    Add Nos class

---
+++ Assets/Projects/Scripts/Car/Movements/Nos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFS.Car.Movements
+{
+    public class Nos
+    {
+        private float torquePower;
+        private float NosCapacity;
+
+        public Nos(float power, float capacity)
+        {
+            torquePower = power;
+            NosCapacity = capacity;
+        }
+
+        public float GetTorquePower()
+        {
+            return torquePower;
+        }
+
+        public float GetNosCapacity()
+        {
+            return NosCapacity;
+        }
+    }
+}
