efc464cb7b9886a5ae19ffaf637b2b79cf4e1392
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2021-02-18 18:15:31 +0700

    Add Car Movement.cs

---
+++ Assets/Projects/Scripts/Car/Movements/CarMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using UnityEngine;
+
+namespace NFS.Car.Movements
+{
+    public class CarMovement : MonoBehaviour
+    {
+        public GearEngine gearEngine;
+        public WheelDrive wheelDrive;
+        public Nos nos;
+
+        private void Start()
+        {
+            
+        }
+        private void FixedUpdate()
+        {
+            
+        }
+    }
+}
