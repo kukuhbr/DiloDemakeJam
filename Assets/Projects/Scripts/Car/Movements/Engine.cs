31cb18fd6fe9f81e92d64479a183caa45cb3f1f0
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2021-02-18 15:55:19 +0700

    Add Engine

---
+++ Assets/Projects/Scripts/Car/Movements/Engine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFS.Car.Movements
+{
+    public class Engine
+    {
+        private float hP;
+
+        public Engine(float horsePower)
+        {
+            hP = horsePower;
+        }
+
+        public float GetHorsePower()
+        {
+            return hP;
+        }
+
+        public void SetHorsePower(float horsePower)
+        {
+            hP = horsePower;
+        }
+    }
+}
