939af56db2e65e5b4d44ff470a15265c420d8b1d
Kukuh <kukuhis777@gmail.com>
2021-02-21 01:17:35 +0700

    add building spawner, modify building root position

---
+++ Assets/Projects/Scripts/Utility/BuildingSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] buildingList;
+    [SerializeField] private Transform buildingContainer;
+    private GameObject[] tracks;
+    public float padding;
+    void Start()
+    {
+        tracks = GameObject.FindGameObjectsWithTag("Track");
+        foreach (GameObject obj in tracks) {
+            SpawnBuilding(obj.transform);
+        }
+    }
+
+    private int randomBuilding;
+    Quaternion orientation;
+    void SpawnBuilding(Transform anchor) {
+        Vector3 buildingPos = anchor.position;
+        randomBuilding = Random.Range(0, buildingList.Length);
+        orientation = Quaternion.identity;
+        Instantiate(buildingList[randomBuilding], buildingPos + anchor.right * -padding, orientation, buildingContainer);
+        randomBuilding = Random.Range(0, buildingList.Length);
+        orientation *= Quaternion.Euler(0, 180, 0);
+        Instantiate(buildingList[randomBuilding], buildingPos + anchor.right * padding, orientation, buildingContainer);
+    }
+}
---
+++ Assets/Projects/Scripts/Utility/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followObject;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 target = followObject.position;
+        target += new Vector3 (0, 1.5f, -5f);
+        transform.position = target;
+    }
+}
