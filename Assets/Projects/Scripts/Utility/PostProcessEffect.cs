56ffa89f7460762fd5058940ede85a188046bb17
Kukuh <kukuhis777@gmail.com>
2021-02-22 00:55:00 +0700

    add post processing effects

---
+++ Assets/Projects/Scripts/Utility/PostProcessEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NFS.Car.Movements;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessEffect : MonoBehaviour
+{
+    PostProcessVolume volume;
+    ChromaticAberration chromatic;
+    LensDistortion lensDistortion;
+    GearEngine gearEngine;
+    void Start()
+    {
+        volume = GetComponent<PostProcessVolume>();
+        gearEngine = GetComponentInParent<GearEngine>();
+        volume.profile.TryGetSettings<LensDistortion>(out lensDistortion);
+        volume.profile.TryGetSettings<ChromaticAberration>(out chromatic);
+    }
+
+    void Update()
+    {
+        float t = gearEngine.GetCurrentSpeed() / gearEngine.GetMaxSpeed();
+        Debug.Log(t);
+        chromatic.intensity.value = Mathf.Lerp(0, 1f, t);
+        lensDistortion.intensity.value = Mathf.Lerp(0, -80, t);
+    }
+}
