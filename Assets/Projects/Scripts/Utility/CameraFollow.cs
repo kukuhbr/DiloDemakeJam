edf55c6b0140ceb4ccff8d08b1f208a166149acb
Kukuh <kukuhis777@gmail.com>
2021-02-21 09:05:52 +0700

    add on progress camera follow script

--- Assets/Projects/Scripts/Utility/CameraFollow.cs
+++ Assets/Projects/Scripts/Utility/CameraFollow.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform followObject;
+    public float yDistance;
+    public float maxZDistance;
+    public float zDistance;
+    public Vector3 rotationOffset;
+    public float cameraCatchUpSpeed;
+    private float _catchUpSpeed;
     void Start()
     {
 
@@ -13,7 +19,30 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         Vector3 target = followObject.position;
-        target += new Vector3 (0, 1.5f, -5f);
-        transform.position = target;
+        if (Input.GetButton("Mirror")) {
+            transform.position = target + followObject.up * yDistance + followObject.forward * 4f;
+
+        } else {
+            target += followObject.up * yDistance + followObject.forward * -zDistance;
+            if (Input.GetButtonUp("Mirror")) {
+                transform.position = target;
+            } else {
+                Vector3 diff = target - transform.position;
+                if (diff.magnitude > 0.1f) {
+                    if (diff.magnitude > maxZDistance) {
+                        _catchUpSpeed = cameraCatchUpSpeed * 4;
+                        //transform.position = target;
+                    } else {
+                        _catchUpSpeed = cameraCatchUpSpeed;
+                    }
+                    transform.position += diff.normalized * Time.deltaTime * _catchUpSpeed;
+                }
+
+                //transform.position = Vector3.Slerp(transform.position, target, Time.fixedDeltaTime * cameraCatchUpSpeed);
+                //transform.position = target;
+            }
+        }
+        transform.LookAt(followObject);
+        transform.rotation = transform.rotation * Quaternion.Euler(rotationOffset);
     }
 }
